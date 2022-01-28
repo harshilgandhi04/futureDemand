5a3e8944fe8cc7002a10336c3b4c8348b71fd505
Harshil Gandhi <harshilgandhi04@gmail.com>
2022-01-28 20:58:39 +0100

    moved create table script to different folder

--- app/futureDemandCoding.py
+++ app/futureDemandCoding.py
@@ -23,18 +23,18 @@ try:
     #Extract events 
     eventsList = soupObj.select('div[id^="event_id_"]')
 
-    create_eventsMetadata_query = """ CREATE TABLE IF NOT EXISTS EventsMetadata(
-        Event_Id  BIGINT NOT NULL  ,
-        Title     VARCHAR(100) NULL,
-        Artists   VARCHAR(100) NULL,
-        Works     VARCHAR(100) NULL,
-        ImageLink VARCHAR(500) NULL,
-        Location  VARCHAR(100) NULL,
-        EventDate DATE NULL        ,
-        EventTime VARCHAR(50) NULL ,
-        CreatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP);
-        """
-    cursor.execute(create_eventsMetadata_query)
+#     create_eventsMetadata_query = """ CREATE TABLE IF NOT EXISTS EventsMetadata(
+#         Event_Id  BIGINT NOT NULL  ,
+#         Title     VARCHAR(100) NULL,
+#         Artists   VARCHAR(100) NULL,
+#         Works     VARCHAR(100) NULL,
+#         ImageLink VARCHAR(500) NULL,
+#         Location  VARCHAR(100) NULL,
+#         EventDate DATE NULL        ,
+#         EventTime VARCHAR(50) NULL ,
+#         CreatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP);
+#         """
+#     cursor.execute(create_eventsMetadata_query)
 
     event_info = []
 
